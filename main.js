9f4d109bf150be5562d48e32480226a51df52c9d
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-26 14:48:28 -0600

    2 3:40

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let tasksEl = document.getElementById('tasks');
 
+// Global Variables
+let tasks = [];
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -25,7 +28,10 @@ function goBtnHandler() {
 
 // MENU FUNCTIONS
 function addTask() {
-  console.log('Add Task');
+  let description = prompt("Enter task description:");
+  tasks.push(newTask(description));
+  saveTasks();
+  displayAll();
 }
 
 function toggleTask() {
@@ -38,4 +44,35 @@ function removeTask() {
 
 function clearAll() {
   console.log('Clear All');
+}
+
+// HELPER FUNCTIONS
+// Return a new task object
+function newTask(taskDescription) {
+  return {
+    description: taskDescription,
+    completed: '',
+  };
+}
+
+// Display All tasks in global tasks array
+function displayAll() {
+  let outputStr = '';
+  for (let i = 0; i < tasks.length; i++) {
+    outputStr += getTaskHTMLStr(tasks[i], i);
+  }
+  tasksEl.innerHTML = outputStr;
+}
+
+// Get html for given task
+function getTaskHTMLStr(task, i) {
+  return `
+    <div>
+      ${i}; ${task.description}
+    </div>
+  `;
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
